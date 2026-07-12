7108dec5b8055039a2979d033746f7f67177dc2f
L1zi-x <lzx1205237865@gmail.com>
2026-07-12 20:47:12 +0800

    第一个独立自主写的代码，关于英尺英寸与厘米的转换

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{	
+int a;
+int b;
+ 
+	printf("Ó¢³ß");
+	scanf("%d", &a);
+	
+	printf("Ó¢´ç");
+	scanf("%d",&b);
+		
+	printf("(%d + %d/12) * 0.3048= %f\n",a ,b ,(a + b/12.0) * 0.3048);
+	
+	return 0;
+}
+
+
