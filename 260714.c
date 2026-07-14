f5aa31c6121b936d23e83417e5c777538aa50ac6
L1zi-x <lzx1205237865@gmail.com>
2026-07-14 10:52:15 +0800

    学习了if语句，试着用新方法计算时间差

---
+++ 260714.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int hour1,minute1;
+    int hour2,minute2;
+    
+    scanf("%d %d",&hour1,&minute1);
+    scanf("%d %d",&hour2,&minute2);
+    int ih =hour2-hour1;
+    int im = minute1-minute2;
+    if(im<0){
+	im = 60+im;
+	ih--;
+	}
+	printf("时间差是%d小时%d分.\n",ih,im);
+    return 0;
+}
