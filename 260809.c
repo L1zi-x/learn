f6c60cead702614614389abbeeeb81fb64c61124
L1zi-x <lzx1205237865@gmail.com>
2026-08-09 21:07:46 +0800

    动态内存分配

---
+++ 260809.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int number;
+    int* a;
+    int i; 
+
+    printf("请输入数量：");
+    scanf("%d", &number);
+
+
+    a = (int*)malloc(number * sizeof(int));
+    if (a == NULL)
+    {
+        printf("内存分配失败！\n");
+        return 1;
+    }
+
+    printf("请依次输入%d个整数：\n", number);
+    for (i = 0; i < number; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("逆序输出：");
+    for (i = number - 1; i >= 0; i--)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+
+    free(a);  
+
+    return 0;
+}
